b75b9c66f5fcbd0ed607b0cd62162f74f4f28751
Slumrag <mrgreenthecristal@yandex.ru>
2025-03-22 18:20:05 +0300

    fix: :alien: update api schema

---
+++ src/api/schema/ApiUser.ts
@@ -0,0 +1,10 @@
+export type ApiUser = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  email: string;
+  department: string;
+  company: string;
+  jobTitle: string;
+};
--- src/api/types/ApiUser.ts
+++
@@ -1,7 +0,0 @@
-export type ApiUser = {
-  id: 0;
-  name: string;
-  department: string;
-  company: string;
-  job_title: string;
-};
