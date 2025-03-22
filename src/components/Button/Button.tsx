ac0ac71b310ec0d9ef97094b527f09d997e77d23
Slumrag <mrgreenthecristal@yandex.ru>
2025-03-22 20:04:04 +0300

    feat: add Button outline variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,23 @@
 import { default as cn } from 'classnames';
 
-import React, { ComponentProps } from 'react';
+import React, { ComponentPropsWithRef } from 'react';
 import classes from './Button.module.scss';
 
-export type ButtonProps = ComponentProps<'button'>;
+export type ButtonProps = ComponentPropsWithRef<'button'> & {
+  variant?: 'solid' | 'outline';
+};
 
-const Button: React.FC<ButtonProps> = ({ className, children, ...props }) => {
+const Button: React.FC<ButtonProps> = ({ className, children, variant = 'solid', ...props }) => {
   return (
-    <button className={cn(classes.Button, className)} {...props}>
+    <button
+      className={cn(
+        classes.Button,
+        { [classes.Button_solid]: variant === 'solid' },
+        { [classes.Button_outline]: variant === 'outline' },
+        className
+      )}
+      {...props}
+    >
       {children}
     </button>
   );
