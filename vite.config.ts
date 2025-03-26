aa3b98bdbfe3c3e128cb6b22d80148bbfa780a12
Slumrag <mrgreenthecristal@yandex.ru>
2025-03-26 20:06:00 +0300

    chore: :wrench: add fallback api base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsConfigPaths from 'vite-tsconfig-paths';
-
+const defaultApiBase = 'http://localhost:3000';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_BASE_URL,
+          target: env.VITE_API_BASE_URL ?? defaultApiBase,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
