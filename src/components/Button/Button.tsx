145c1cc56c2d052f0477fac0cd44767b6668b0f4
Slumrag <mrgreenthecristal@yandex.ru>
2025-03-26 19:29:18 +0300

    feat: add Button loading state

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,15 +5,24 @@ import classes from './Button.module.scss';
 
 export type ButtonProps = ComponentPropsWithRef<'button'> & {
   variant?: 'solid' | 'outline';
+  loading?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ className, children, variant = 'solid', ...props }) => {
+const Button: React.FC<ButtonProps> = ({
+  className,
+  children,
+  variant = 'solid',
+  loading = false,
+  ...props
+}) => {
   return (
     <button
       className={cn(
         classes.Button,
         { [classes.Button_solid]: variant === 'solid' },
         { [classes.Button_outline]: variant === 'outline' },
+        { [classes.Button_loading]: loading },
+
         className
       )}
       {...props}
